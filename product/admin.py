3082a0ca6bda107dbbeda83d473c9b163c43eca6
Lina <lina@Linas-MacBook-Pro.local>
2026-04-16 00:28:51 +0300

    Add product variants and image model

--- product/admin.py
+++ product/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, ProductVariant, Image
 
-admin.site.register(Product)
+
+class ProductVariantInline(admin.TabularInline):
+    model = ProductVariant
+    extra = 1  
+
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 3 
+    
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_filter = ['features']
+    search_fields = ['name']
+    inlines = [ProductVariantInline, ImageInline]  
+
--- product/models.py
+++ product/models.py
@@ -4,12 +4,49 @@ class Product(models.Model):
     name            = models.CharField(max_length=200, null=False, verbose_name='Product Name')
     features        = models.TextField(null=False, default='')
     certificate     = models.TextField(null=False, default='')
-    image           = models.ImageField(upload_to='products/', blank=True, null=True)
-    height          = models.IntegerField()
-    depth           = models.IntegerField()
-    width           = models.IntegerField()
-    color           = models.CharField(max_length=200)
-
+    
+   
     def __str__(self):
         return self.name
+    
+    
+class ProductVariant(models.Model):   
 
+    HEIGHT_CHOICES = [
+        (6, '6U'), (9, '9U'), (12, '12U'),
+        (15, '15U'), (18, '18U'), (21, '21U'),
+    ]
+
+    DIMENSION_CHOICES = [
+        (600, '600mm'),
+        (800, '800mm'),
+    ]
+
+    COLOR_CHOICES = [
+        ('RAL7035', 'Light Gray (RAL 7035)'),
+        ('custom', 'Custom on Request'),
+    ]
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='variants'
+    )
+
+    height_u = models.PositiveSmallIntegerField(choices=HEIGHT_CHOICES)
+    width_mm = models.PositiveIntegerField(choices=DIMENSION_CHOICES)
+    depth_mm = models.PositiveIntegerField(choices=DIMENSION_CHOICES)
+
+    color     = models.CharField(max_length=20, choices=COLOR_CHOICES, default='RAL7035')
+    sku       = models.CharField(max_length=100, unique=True, blank=True)  # e.g. GAMMA-6U-600-600
+    is_active = models.BooleanField(default=True)
+
+class Image(models.Model):  
+    name   = models.CharField(max_length=100, blank=True) 
+    image  = models.ImageField(upload_to='products/', blank=True, null=True)
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='image'
+    )
